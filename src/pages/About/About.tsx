cb78cf8d285a702ae50dde996dd26eace28428d3
Kohei Hasegawa <ameutau@gmail.com>
2017-12-04 21:04:06 +0900

    Add moment

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import Navi from '../../Navi';
+import * as moment from 'moment';
 
 export default () => (
   <div>
     <Navi />
     <h2>About</h2>
+    <p>{moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
   </div>
 );
